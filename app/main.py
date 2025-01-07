be29826f1dc2f419fc597badb12e8689b9666719
Kopciu90 <piotrekkopson@wp.pl>
2025-01-07 22:15:59 +0100

    add new route to test CI Workflow

--- app/main.py
+++ app/main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return '<h1>Hello World</h1>'
 
+@app.route('/devops')
+def hello_world():
+    return '<h1>Devops Test</h1>'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
